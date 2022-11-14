b07a7a67b06312110373312da8872226ae69949c
mshereef03 <mshereef2003@gmail.com>
2022-11-14 21:40:35 +0200

    block1

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/login',(req,res)=>{
+    console.log(req.socket.remoteAddress);
     if(req.socket.remoteAddress=='::ffff:102.191.234.36')return res.end();
     if(req.session.isAuth)return res.redirect('/home');
     else res.render('login');
